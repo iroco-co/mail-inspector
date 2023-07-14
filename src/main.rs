4e7111a7e8ccdac0b2a1926de065d8a25dce6828
Bruno Thomas <bruno@barreverte.fr>
2023-07-14 11:49:35 +0200

    feat: implement attachments count

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 use clap::{Args, Parser, Subcommand};
-use mail_parser::{HeaderValue, Message};
+use mail_parser::{HeaderValue, Message, MimeHeaders};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -33,10 +33,31 @@ fn main() {
     }
 }
 
-fn count_recipients(email: Option<PathBuf>) {
-    let email_content = fs::read(email.as_ref().unwrap()).expect("Failed to read email file");
-    let mail = Message::parse(&email_content).expect("Failed to parse email file");
-    println!("{:?} {:?}", email.as_ref().unwrap(), size(mail.to()) + size(mail.cc()));
+fn count_recipients(email_path: Option<PathBuf>) {
+    let email_content = fs::read(email_path.as_ref().unwrap()).expect("Failed to read email file");
+    let email = Message::parse(&email_content).expect("Failed to parse email file");
+    println!("{:?} {:?}", email_path.as_ref().unwrap(), size(email.to()) + size(email.cc()));
+}
+
+fn count_attachments(email_path: Option<PathBuf>) {
+    let email_content = fs::read(email_path.as_ref().unwrap()).expect("Failed to read email file");
+    let email = &Message::parse(&email_content).unwrap();
+    let mut nb = 0;
+    let mut size = 0;
+    print_attachments(&email_path, &email, &mut nb, &mut size);
+    println!("{:?} {:?} {:?}", email_path.as_ref().unwrap(), nb, size);
+}
+
+fn print_attachments(email_path: &Option<PathBuf>, email: &Message, nb_attachments: &mut i32, total_size: &mut usize) {
+    for attachment in email.attachments() {
+        if !attachment.is_message() {
+            println!("{:?} {:?} {:?}", email_path.as_ref().unwrap(), attachment.attachment_name().unwrap_or("Untitled"), attachment.len());
+            *nb_attachments += 1;
+            *total_size += attachment.len();
+        } else {
+            print_attachments(email_path, attachment.message().unwrap(), nb_attachments, total_size);
+        }
+    }
 }
 
 fn size(addr_list: &HeaderValue) -> usize {
@@ -45,8 +66,4 @@ fn size(addr_list: &HeaderValue) -> usize {
         HeaderValue::Address(_addresses) => 1,
         _ => 0,
     }
-}
-
-fn count_attachments(email: Option<PathBuf>) {
-    println!("count attachments {:?}", email)
 }
